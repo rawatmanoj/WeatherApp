7debfc0a40ba45c12860ca7452026a8c1636860c
rawatmanoj <42403468+rawatmanoj@users.noreply.github.com>
2020-06-27 10:58:59 +0530

    update

--- app.js
+++ app.js
@@ -1,69 +1,63 @@
-// window.addEventListener("load",() => {
+// window.addEventListener("load", () => {
+//   let long;
+//   let lat;
+//   let temperatureDescrtiption = document.querySelector(
+//     ".temperature-description"
+//   );
+//   let temperatureDegree = document.querySelector(".temperature-degree");
+//   let locationTimezone = document.querySelector(".location-timezone");
 
-// let long;
-// let lat;
-// let temperatureDescrtiption=document.querySelector('.temperature-description');
-// let temperatureDegree=document.querySelector('.temperature-degree');
-// let locationTimezone=document.querySelector('.location-timezone');
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition((position) => {
+//       //console.log(position);
+//       long = position.coords.longitude;
+//       lat = position.coords.latitude;
 
-// if(navigator.geolocation){
+//       const proxy = "https://cors-anywhere.herokuapp.com/";
 
-// navigator.geolocation.getCurrentPosition(position =>{
+//       const api = `${proxy}https://api.darksky.net/forecast/7c546e6f35b69ff327cb384c8f7a721c/${lat},${long}`;
 
-//     //console.log(position);
-//     long=position.coords.longitude;
-//     lat=position.coords.latitude;
+//       fetch(api)
+//         .then((response) => {
+//           //console.log(response);
+//           return response.json();
+//         })
+//         .then((data) => {
+//           //console.log(data);
+//           const { temperature, summary, icon } = data.currently;
 
-//     const proxy='https://cors-anywhere.herokuapp.com/';
+//           temperatureDegree.textContent = temperature;
+//           temperatureDescrtiption.textContent = summary;
+//           locationTimezone.textContent = data.timezone;
 
-//     const api=`${proxy}https://api.darksky.net/forecast/7c546e6f35b69ff327cb384c8f7a721c/${lat},${long}`;
-
-//      fetch(api)
-//     .then(response => {
-//       //console.log(response);
-//         return response.json();
-//     })
-//     .then(data=>{
-//         //console.log(data);
-//        const {temperature,summary,icon}= data.currently;
-
-//        temperatureDegree.textContent=temperature;
-//        temperatureDescrtiption.textContent=summary;
-//        locationTimezone.textContent=data.timezone;
-
-//         setIcons(icon,document.querySelector(".icon"));
-
+//           setIcons(icon, document.querySelector(".icon"));
+//         });
 //     });
 
-// });
+//     //do this later
 
-// //do this later
+//     const proxy = "https://cors-anywhere.herokuapp.com/";
 
-// const proxy='https://cors-anywhere.herokuapp.com/';
+//     const api1 = `${proxy}http://ipinfo.io`;
 
-// const api1=`${proxy}http://ipinfo.io`;
+//     fetch(api1)
+//       .then((response1) => {
+//         //console.log(response1);
+//         return response1.json();
+//       })
+//       .then((data1) => {
+//         // console.log(JSON.stringify(data1));
+//         return JSON.stringify(data1);
+//       })
+//       .catch((response) => {
+//         console.log(response);
+//       });
+//   }
 
-// fetch(api1)
-// .then(response1 =>{
-//   //console.log(response1);
-//    return response1.json();
-// })
-// .then(data1 =>{
-//  // console.log(JSON.stringify(data1));
-//   return JSON.stringify(data1);
-// }).catch(response=>{
-//   console.log(response);
-// });
-
-// }
-
-// function setIcons(icon,iconId){
-
-//    const skycons=new Skycons({color:"white"})
-//    const currentIcon=icon.replace(/-/g,"_").toUpperCase();
-//    skycons.play();
-//    return skycons.set(iconId,Skycons[currentIcon]);
-
-// }
-
+//   function setIcons(icon, iconId) {
+//     const skycons = new Skycons({ color: "white" });
+//     const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+//     skycons.play();
+//     return skycons.set(iconId, Skycons[currentIcon]);
+//   }
 // });
